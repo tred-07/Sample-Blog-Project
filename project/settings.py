c5517514919f74885f632fa4e61e5e77942b8eb5
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-13 20:25:56 +0600

    upd

--- project/settings.py
+++ project/settings.py
@@ -10,14 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
-import environ
 
 # from dotenv import load_dotenv, dotenv_values
 # load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
