26f1d584e7068f377caafc84a37696fcd7125cdc
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-13 20:03:29 +0600

    debugging

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 # from dotenv import load_dotenv, dotenv_values
 # load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
