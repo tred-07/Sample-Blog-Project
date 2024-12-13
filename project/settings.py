d6223e3e681260f2631cffa03d2d9bb57c01f4f9
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-13 20:08:59 +0600

    upd requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
 import environ
-env = environ.Env()
-environ.Env.read_env()
+
 # from dotenv import load_dotenv, dotenv_values
 # load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
